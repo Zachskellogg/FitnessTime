879927401165c4d1ee9a65beb1badb4182ec1d85
ZachKellogg <zachskellogg@gmail.com>
2020-07-25 14:15:15 -0600

    addint credts to the Mongo

--- server.js
+++ server.js
@@ -13,11 +13,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://EMpDB:9jDrbFhNAeyJ4!b@ds119578.mlab.com:19578/heroku_j6krq6t0", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
+// 9jDrbFhNAeyJ4!b
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
